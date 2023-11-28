aee3db4375521f1f726baf042cfabdf581e7e34f
Muhammad Haris <muhammad.haris1@outlook.com>
2023-11-28 17:54:04 +0100

    Basic react project setup with vite

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import words from "./wordList.json";
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState<string>(
+    words[Math.floor(Math.random() * words.length)]
+  );
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+
+
+  return (
+    <div style={{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2rem",
+      margin: "0 auto",
+      alignItems: "center",
+    }}>
+      <div style={{
+        fontSize: "2rem", textAlign: "center"
+      }}>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
